e6fd9b5d2c3704bc53ce53b7c724c422b02aa369
Muhammad Dani <156415805+mdaninas@users.noreply.github.com>
2024-05-15 15:29:20 +0700

    pertemuan10

--- src/App.vue
+++ src/App.vue
@@ -1,355 +1,285 @@
-<script setup>
-import { ref, computed } from 'vue'
-
-let id = 0
-
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([
-  { id: id++, text: 'Makan', done: true },
-  { id: id++, text: 'Gosok Gigi', done: true },
-  { id: id++, text: 'Cuci Motor', done: false },
-  { id: id++, text: 'Meeting Jam 18:00', done: false }
-])
-
-const filteredTodos = computed(() => {
-  return hideCompleted.value
-    ? todos.value.filter((t) => !t.done)
-    : todos.value
-})
-
-function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
-}
-
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
-}
-</script>
-
 <template>
   <div class="containerblue">
-    <div class="purple">
-      <h2 class="addtitle">TAMBAHKAN LIST ANDA</h2>
-      <form @submit.prevent="addTodo">
-        <input class="box" v-model="newTodo" required placeholder="new todo">
-        <button class="button-20 buttonfirst">Tambah</button>
-      </form>
-    </div>
     <div class="red">
       <div class="black">
-        <img class="icon" src="./assets/list.gif" alt="">
+        <h1> MY APP <span></span></h1>
       </div>
+      <div class="list">
+        <div class="li1">
+          <div class="li2">
+            <h2 class="addtitle">Add new todo...</h2>
+            <input v-model="todo" type="text" class="box" @keyup.enter="add" />
+          </div>
+          <div>
+            <button class="button-20" @click="add">ADD</button>
+          </div>
+        </div>
+        <small class="titillium-web-regular">Total TODO : {{ totalTODO }}</small>
+        <ul class="list-group mt-3">
+          <li class="list-group-item d-flex justify-content-between align-items-center" v-for="(item, index) in todos" :key="index" :class="{ 'completed': item.completed }">
+            <span v-if="!item.completed">{{ item.text }}</span>
+            <span v-else class="done">{{ item.text }}</span>
+            <div>
+              <button class="button-29" @click="remove(index)">X</button>
+              <button class="button-28" @click="toggleCompletion(index)">{{ item.completed ? 'Undone' : 'Done' }}</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="purple">
       <div class="blue">
-        <h1><span style="color: white;">MY</span><span style="color: cadetblue;">LIST</span></h1>
+        <h2>Posts</h2>
       </div>
       <div class="yellow">
-        <div class="list">
-          <ul>
-            <li class="alllist" v-for="todo in filteredTodos" :key="todo.id">
-              <div class="li1">
-                <input type="checkbox" v-model="todo.done">
-                <span :class="{ done: todo.done }" class="titillium-web-regular listtext">{{ todo.text }}</span>
-              </div>
-              <div class="li2">
-                <button class="button-28 listbut" @click="removeTodo(todo)">Hapus</button>
-              </div>
-            </li>
-          </ul>
-          <button class="filter button-29" @click="hideCompleted = !hideCompleted">
-            {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-          </button>
+        <div class="form-group">
+          <label for="postUser" class="titillium-web-regular">User</label>
+          <select class="box" v-model="newPost.userId" id="postUser" @change="fetchPosts">
+            <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+          </select>
+        </div>
+        <hr>
+        <div v-if="loadingPosts" class="listtext">Loading posts...</div>
+        <div v-else>
+          <div v-for="post in posts" :key="post.id" class="card mb-3">
+            <div class="card-body">
+              <h5 class="card-title">{{ post.title }}</h5>
+              <p class="card-text">{{ post.body }}</p>
+              <p class="card-text">User: {{ getUserById(post.userId).name }}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-*{
+
+<script>
+export default {
+  data() {
+    return {
+      todo: '',
+      todos: [],
+      activeTab: 'todos',
+      newPost: {
+        title: '',
+        body: '',
+        userId: null
+      },
+      posts: [],
+      users: [],
+      loadingPosts: false
+    };
+  },
+  computed: {
+    totalTODO() {
+      return this.todos.length;
+    }
+  },
+  methods: {
+    add() {
+      if (this.todo.trim() !== '') {
+        this.todos.push({ text: this.todo, completed: false });
+        this.todo = '';
+      }
+    },
+    remove(index) {
+      this.todos.splice(index, 1);
+    },
+    toggleCompletion(index) {
+      this.todos[index].completed = !this.todos[index].completed;
+    },
+    showTodos() {
+      this.activeTab = 'todos';
+    },
+    showPosts() {
+      this.activeTab = 'posts';
+      this.fetchPosts();
+    },
+    fetchPosts() {
+      if (!this.newPost.userId) return; // Return if no user is selected
+      this.loadingPosts = true;
+      fetch(`https://jsonplaceholder.typicode.com/posts?userId=${this.newPost.userId}`)
+        .then(response => response.json())
+        .then(data => {
+          this.posts = data;
+          this.loadingPosts = false;
+        })
+        .catch(error => {
+          console.error('Error fetching posts:', error);
+          this.loadingPosts = false;
+        });
+    },
+    createPost() {
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(this.newPost),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Post created successfully:', data);
+        this.posts.unshift(data);
+        this.newPost.title = '';
+        this.newPost.body = '';
+        this.newPost.userId = null;
+      })
+      .catch(error => {
+        console.error('Error creating post:', error);
+      });
+    },
+    fetchUsers() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+          this.users = data;
+        })
+        .catch(error => {
+          console.error('Error fetching users:', error);
+        });
+    },
+    getUserById(id) {
+      return this.users.find(user => user.id === id);
+    }
+  },
+  mounted() {
+    this.fetchUsers();
+  }
+};
+</script>
+
+
+<style scoped>
+
+* {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
 }
-form{
+
+
+form {
   display: flex;
   flex-direction: column;
 }
-li{
+
+
+li {
   list-style-type: none;
   display: flex;
   justify-content: space-between;
 }
-.kanit-semibold {
+
+
+h1 {
   font-family: "Kanit", sans-serif;
   font-weight: 600;
   font-style: normal;
+  font-size: 120px;
 }
-.addtitle{
-      font-family: "IBM Plex Mono", monospace;
-      font-size: 40px;
-      font-weight: 400;
-      font-style: normal;
-}
-div.li1{
-  display: flex;
-  margin-left: 50px;
-  gap: 30px;
-}
-div.li2{
-  padding-right: 50px;
-}
-.filter{
-  position: relative;
-  left: 25%;
-  bottom: 40px;
-  position: relative;
-  width: 100%;
-}
-  .titillium-web-regular {
-      font-family: "Titillium Web", sans-serif;
-      font-weight: 400;
-      font-size: 30px;
-      font-style: normal;
-    }
-    input[type="checkbox"] {
-     position: relative;
-     right: 50px;
-     top: 15px;
-     scale: 2.5;
-     margin-left: 10px;
-    }
-.list{
-  position: absolute;
-  height: 85%;
-  width: 80%;
-  top: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
 
+
+h1 span {
+  margin: 40px;
 }
-.containerblue{
+
+
+.containerblue {
   background-image: url(./assets/2825710.gif);
   background-repeat: no-repeat;
   background-size: cover;
   height: 100vh;
   display: flex;
   justify-content: space-between;
-  flex-direction: row;
+  align-items: center; 
+  padding: 0 10%; 
 }
-.listtext{
-  position: relative;
-  top: 13px;
-}
-.listbut {
-  position: relative;
-  top: 15px;
-}
-h1{
-      font-family: "Kanit", sans-serif;
-      font-weight: 600;
-      font-style: normal;
-      font-size: 120px;
-}
-h1 span{
-  margin: 40px;
-}
-.containerblue::before{
-  content: "";
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 120%;
-  background: rgba(0, 0, 0, 0.6);
-}
-
 
 .red {
-  position: relative;;
   display: flex;
   flex-direction: column;
-  height: 100%;
   width: 40%;
   border-radius: 20px;
-}
-.purple {
-  top: 22%;
-  left: 10%;
-  position: relative;
   background: rgba(255, 255, 255, 0.6);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 20%;
-  width: 30%;
-  border-radius: 15px;
-  justify-content: space-around;
-}
-.black{
-  position:absolute;
-  top: 0;
-  left: 0;
-  background-color: white;
-  height: 15%;
-  width: 20%;
-  border-top-left-radius: 20px;
-  overflow: hidden;
-  
-  }
-.blue{
-  position: absolute;
-  background-color: black;
-  color: white;
-    top: 0;
-    right: 0;
-    height: 15%;
-    width: 80%;
-    border-top-right-radius: 15px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 20px;
-  
+  padding: 20px; 
 }
 
-.yellow {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  height: 85%;
+.gg{
+  padding: 2px;
+}
+.purple {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 40%;
+  border-radius: 20px;
+  background: rgba(255, 255, 255, 0.6); /* Menggeser latar belakang di sini */
+  padding: 20px; /* Padding untuk jarak antara elemen */
+}
+
+/* Gaya untuk Header Bagian Todo (Hitam) */
+.black {
+  position: relative;
+  background-color: white;
+  height: 15%;
   width: 100%;
+  border-top-left-radius: 20px;
+  overflow: hidden;
+  margin-bottom: 20px; /* Menggeser header dari konten */
+}
+
+/* Gaya untuk Header Bagian Posting (Biru) */
+.blue {
+  background-color: black;
+  color: white;
+  height: 15%;
+  width: 100%;
+  border-top-right-radius: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 20px;
+  margin-bottom: 20px; /* Menggeser header dari konten */
+}
+
+/* Gaya untuk Bagian Konten Posting (Kuning) */
+.yellow {
+  height: 85%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
   backdrop-filter: blur(10px);
   border-bottom-left-radius: 200px;
-  background: rgba(255, 255, 255, 0.5) ;
-  box-shadow: 0px 0px 16px 0px rgba(255,255,255, 0.5);
+  background: rgba(255, 255, 255, 0.5);
+  box-shadow: 0px 0px 16px 0px rgba(255, 255, 255, 0.5);
 }
-li{
+
+/* Gaya untuk List Item */
+li {
   padding-top: 20px;
 }
-ul{
+
+/* Gaya untuk List */
+ul {
   height: 80%;
   overflow: auto;
 }
-img.icon{
-  width: 100%;
-  height: 100%;
-  border-top-left-radius: 15px;
-  
-}
-.done {
+
+/* Gaya untuk Tanda Check pada Item Selesai */
+.completed {
   text-decoration: line-through;
 }
 
-.button-28 {
-  border: 2px solid #1A1A1A;
-  border-radius: 15px;
-  box-sizing: border-box;
-  color: #3B3B3B;
-  cursor: pointer;
-  display: inline-block;
-  font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: 16px;
-  font-weight: 600;
-  line-height: normal;
-  margin: 0;
-  height: 40px;
-  min-width: 0;
-  outline: none;
-  padding: 0 24px;
-  text-align: center;
-  text-decoration: none;
-  transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  width: 100%;
-  will-change: transform;
-}
-
-.button-28:disabled {
-  pointer-events: none;
-}
-
-.button-28:hover {
-  color: #fff;
-  background-color: #1A1A1A;
-  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
-  transform: translateY(-2px);
-}
-
-.button-28:active {
-  box-shadow: none;
-  transform: translateY(0);
-}
-
-.button-29 {
-  border: 2px solid #1A1A1A;
-  border-radius: 15px;
-  box-sizing: border-box;
-  color: #3B3B3B;
-  cursor: pointer;
-  display: inline-block;
-  font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: 16px;
-  font-weight: 600;
-  line-height: normal;
-  margin: 0;
-  min-height: 60px;
-  min-width: 0;
-  outline: none;
-  padding: 16px 24px;
-  text-align: center;
-  text-decoration: none;
-  transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  width: 50%;
-  will-change: transform;
-}
-
-.button-29:disabled {
-  pointer-events: none;
-}
-
-.button-29:hover {
-  color: #fff;
-  background-color: #1A1A1A;
-  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
-  transform: translateY(-2px);
-}
-
-.button-29:active {
-  box-shadow: none;
-  transform: translateY(0);
-}
-.box{
-  position: relative;
-  bottom: 15px;
-width: 100%;
-color: rgb(36, 35, 42);
-font-size: 16px;
-line-height: 20px;
-min-height: 28px;
-border-radius: 4px;
-padding: 8px 16px;
-border: 2px solid transparent;
-box-shadow: rgb(0 0 0 / 12%) 0px 1px 3px,
-rgb(0 0 0 / 24%) 0px 1px 2px;
-background: rgb(251, 251, 251);
-transition: all 0.1s ease 0s;
-}
-.box:focus {
-  border: 2px solid rgb(124, 138, 255);
-}
+/* Gaya untuk Tombol */
+.button-28,
+.button-29,
 .button-20 {
   border: 2px solid #1A1A1A;
   border-radius: 15px;
@@ -362,7 +292,6 @@ transition: all 0.1s ease 0s;
   font-weight: 600;
   line-height: normal;
   margin: 0;
-  height: 40px;
   min-width: 0;
   outline: none;
   padding: 0 24px;
@@ -372,15 +301,19 @@ transition: all 0.1s ease 0s;
   user-select: none;
   -webkit-user-select: none;
   touch-action: manipulation;
-  width: 50%;
-  margin-left: 25%;
   will-change: transform;
 }
 
+/* Gaya untuk Tombol Saat Dinaktifkan */
+.button-28:disabled,
+.button-29:disabled,
 .button-20:disabled {
   pointer-events: none;
 }
 
+/* Efek Hover pada Tombol */
+.button-28:hover,
+.button-29:hover,
 .button-20:hover {
   color: #fff;
   background-color: #1A1A1A;
@@ -388,8 +321,36 @@ transition: all 0.1s ease 0s;
   transform: translateY(-2px);
 }
 
-.button-2:active {
+/* Efek Ketika Tombol Ditekan */
+.button-28:active,
+.button-29:active,
+.button-20:active {
   box-shadow: none;
   transform: translateY(0);
 }
+
+/* Gaya untuk Input Box */
+.box {
+  position: relative;
+  bottom: 15px;
+  width: 100%;
+  color: rgb(36, 35, 42);
+  font-size: 16px;
+  line-height: 20px;
+  min-height: 28px;
+  border-radius: 4px;
+  padding: 8px 16px;
+  border: 2px solid transparent;
+  box-shadow: rgb(0 0 0 / 12%) 0px 1px 3px,
+              rgb(0 0 0 / 24%) 0px 1px 2px;
+  background: rgb(251, 251, 251);
+  transition: all 0.1s ease 0s;
+}
+
+/* Gaya saat Input Box dalam Fokus */
+.box:focus {
+  border: 2px solid rgb(124, 138, 255);
+}
+
+
 </style>
